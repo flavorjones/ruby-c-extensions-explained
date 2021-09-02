9499bd53546921705f5967b5f98ef16b05078020
Mike Dalessio <mike.dalessio@gmail.com>
2021-09-02 13:54:42 -0400

    clean targets

--- packaged_source/ext/packaged_source/extconf.rb
+++ packaged_source/ext/packaged_source/extconf.rb
@@ -2,8 +2,8 @@ require "mkmf"
 
 $VPATH << "$(srcdir)/yaml"
 $srcs = Dir.glob("#{$srcdir}/{,yaml/}*.c").map { |n| File.basename(n) }.sort
-
 append_cppflags("-I$(srcdir)/yaml")
+
 find_header("yaml.h")
 have_header("config.h") # defines HAVE_CONFIG_H macro
 
