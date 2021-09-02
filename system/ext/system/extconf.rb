704f523a93cd8d215a46e85dcedff25028731e1b
Mike Dalessio <mike.dalessio@gmail.com>
2021-09-02 13:59:33 -0400

    don't need dir-config for the example

--- system/ext/system/extconf.rb
+++ system/ext/system/extconf.rb
@@ -1,7 +1,6 @@
 require "mkmf"
 
-dir_config "libyaml"
-unless find_header('yaml.h') && find_library('yaml', 'yaml_get_version')
+unless find_header("yaml.h") && find_library("yaml", "yaml_get_version")
   abort("\nERROR: *** could not find libyaml development environment ***\n\n")
 end
 
