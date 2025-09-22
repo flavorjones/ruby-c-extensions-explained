7a5adcd1853177a294ee2f423d56d64a73cfe84f
Edouard CHIN <chin.edouard@gmail.com>
2025-09-22 22:49:01 +0200

    Add a `dir_config` for users on MacOS
    - Users on MacOS that installed Ruby through different installer such
      as `ruby-install` will have their Ruby built with this
      `configure_args` `--with-libyaml-dir=/opt/homebrew/opt/libyaml`.
    
      This commit adds a `dir_config("libyaml")` so that the flag is taken
      in consideration. Otherwise the header and function can't be found
      leading to a compilation error.
    
      I'm not super familiar with C extensions, but from reading the doc
      it seems that `dir_config` just "pushes" to the list of directories
      ruby will search for libraries and include files. It shouldn't
      affect users that are not on MacOS.

--- system/ext/system/extconf.rb
+++ system/ext/system/extconf.rb
@@ -1,5 +1,6 @@
 require "mkmf"
 
+dir_config('libyaml')
 unless find_header("yaml.h") && find_library("yaml", "yaml_get_version")
   abort("\nERROR: *** could not find libyaml development environment ***\n\n")
 end
